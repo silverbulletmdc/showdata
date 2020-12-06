c454d6ae91991642283577cabd324e6e61361be8
Dechao Meng <dechao.meng@vipl.ict.ac.cn>
2020-12-06 04:55:01 -0500

    force clear cache

--- showdata/showdata.py
+++ showdata/showdata.py
@@ -66,6 +66,10 @@ def generate_html_table(content_table, image_width='auto', image_height='auto', 
     html += '<head>'
 
     html += """
+        <meta http-equiv="Cache-Control" content="no-cache, no-store, must-revalidate" />
+        <meta http-equiv="Pragma" content="no-cache" />
+        <meta http-equiv="Expires" content="0" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
         <link href="https://cdn.bootcdn.net/ajax/libs/font-awesome/5.15.1/css/all.min.css" rel="stylesheet">
         <link rel="stylesheet" href="https://unpkg.com/bootstrap-table@1.18.0/dist/bootstrap-table.min.css">
